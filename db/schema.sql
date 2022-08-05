b1dec0dc0d44de0709411c8fec8569895c1ef7bd
BlandSchwad <schwaderer@protonmail.com>
2022-08-05 10:03:01 -0700

    Move db file, update example env  Please enter the commit message for your changes. Lines starting

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,8 @@
+CREATE DATABASE reviews;
+\c reviews
+
 CREATE TABLE reviews(
-  id integer NOT NULL,
+  id SERIAL NOT NULL,
   product_id integer NOT NULL,
   rating integer CHECK (rating >= 0 AND rating <= 5)NOT NULL,
   "date" bigint NOT NULL,
@@ -17,7 +20,7 @@ CREATE TABLE reviews(
 \COPY reviews FROM '/data/reviews.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE photos(
-  id integer NOT NULL,
+  id SERIAL NOT NULL,
   review_id integer NOT NULL,
   url varchar(200) NOT NULL,
   CONSTRAINT photos_pkey PRIMARY KEY(id)
@@ -26,7 +29,7 @@ CREATE TABLE photos(
 \COPY photos FROM '/data/review_photos.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE characteristic_reviews(
-  id integer NOT NULL,
+  id SERIAL NOT NULL,
   review_id integer NOT NULL,
   characteristic_id integer NOT NULL,
   "value" integer CHECK(value >= 0 AND value <= 5) NOT NULL,
@@ -36,19 +39,13 @@ CREATE TABLE characteristic_reviews(
 \COPY characteristic_reviews FROM '/data/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE characteristics(
-  id integer NOT NULL,
+  id SERIAL NOT NULL,
   product_id integer NOT NULL,
   "name" varchar(7) NOT NULL,
   CONSTRAINT characteristics_pkey PRIMARY KEY(id)
 );
 
 \COPY characteristics FROM '/data/characteristics.csv' DELIMITER ',' CSV HEADER;
-
-CREATE TABLE products (
-  id integer NOT NULL,
-  CONSTRAINT "products_pkey" PRIMARY KEY(id)
-);
-
 
 ALTER TABLE photos
   ADD CONSTRAINT photos_review_id_fkey
@@ -62,13 +59,7 @@ ALTER TABLE characteristic_reviews
   ADD CONSTRAINT characteristic_reviews_characteristic_id_fkey
     FOREIGN KEY (characteristics_id) REFERENCES characteristics(id);
 
-
-INSERT INTO PRODUCTS SELECT DISTINCT ON (product_id) product_id FROM characteristics;
-
-ALTER TABLE reviews
-  ADD CONSTRAINT reviews_product_id_fkey
-    FOREIGN KEY (product_id) REFERENCES products(id);
-
-ALTER TABLE characteristics
-  ADD CONSTRAINT characteristics_product_id_fkey
-    FOREIGN KEY (product_id) REFERENCES products(id);
+SELECT setval('reviews_id_seq'::regclass, (SELECT MAX(id) FROM reviews));
+SELECT setval('photos_id_seq'::regclass, (SELECT MAX(id) FROM photos));
+SELECT setval('characteristics_id_seq'::regclass, (SELECT MAX(id) FROM characteristics));
+SELECT setval('characteristic_reviews_id_seq'::regclass, (SELECT MAX(id) FROM characteristic_reviews));
